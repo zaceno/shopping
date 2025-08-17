481ad8fa11c14eef43074837cef4d1066b23dcc1
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2025-08-17 22:44:30 +0200

    make app PWA enabled

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import path from "path"
 import { defineConfig } from "vite"
 import hyperapp from "vite-plugin-hyperapp"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
   resolve: {
@@ -10,6 +11,39 @@ export default defineConfig({
     },
   },
   base: "/shopping/",
-  plugins: [hyperapp()],
+  plugins: [
+    hyperapp(),
+    VitePWA({
+      registerType: "autoUpdate", // keeps SW up to date
+      includeAssets: [
+        "icon-sprites.svg",
+        "icons/icon-192.png",
+        "icons/icon-512.png",
+        "icons/icon-180.png", // optional Apple fallback
+      ],
+      manifest: {
+        name: "Shopping List",
+        short_name: "ShopList",
+        description: "A simple shopping list app.",
+        display: "standalone",
+        background_color: "#6495ED",
+        theme_color: "#6495ED",
+        icons: [
+          {
+            src: "icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   test: {},
 })
