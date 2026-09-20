ea2a38145fba9395108887e5e165253d942ec0ab
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2026-09-20 11:22:38 +0200

    feat: add push API endpoint

---
+++ src/api/push.ts
@@ -0,0 +1,25 @@
+import { type Item } from "./items"
+const DEBOUNCE_TIME = 1000
+
+function compareChanges(previous, latest) {}
+
+const delayedChanges = (() => {
+  let delay: Timeout | null = null
+  return function (previous: Item[], latest: Item[]) {
+    if (delay !== null) clearTimeout(delay)
+    delay = setTimeout(() => {
+      delay = null
+      compareChanges(previous, latest)
+    }, DEBOUNCE_TIME)
+  }
+})()
+
+export function pushChanges(
+  _: any,
+  options: {
+    previous: Item[]
+    latest: Item[]
+  },
+) {
+  delayedChanges(options.previous, options.latest)
+}
